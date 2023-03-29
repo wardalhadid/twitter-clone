0e5c4425e3c192581f67f79d25e4bdab139c974f
Ward Alhadid <wf.hadid94@gmail.com>
2023-03-29 00:02:19 -0400

    setup routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -87,8 +88,10 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-2 text-slate-300">
-          <span> {`@${author.username}`} </span>
-          <span className=""> {`. ${dayjs(post.createdAt).fromNow()}`} </span>
+          <Link href={`/@${author.username}`}> <span> {`@${author.username}`} </span> </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin"> {`. ${dayjs(post.createdAt).fromNow()}`} </span>
+          </Link>
         </div>
         <span className="text-xl"> {post.content} </span>
       </div>
